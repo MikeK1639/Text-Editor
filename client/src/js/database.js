e36a7ec180adfff7673d0f1c29da92f4d022813c
MikeK1639 <126822125+MikeK1639@users.noreply.github.com>
2023-05-25 20:27:18 -0400

    refactored and owrking

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,43 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => { console.error('putDb not implemented'); 
-
-const todosDb = await openDB('todos', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = todosDb.transaction('todos', 'readwrite');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('todos');
-
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({ todo: content });
-
-  // Get confirmation of the request.
+// Method that takes some content and adds it to the IndexedDB database using the idb module
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('🚀 - data saved to the database', result.value);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => { console.error('getDb not implemented');
-
-const todosDb = await openDB('todos', 1);
-
-// Create a new transaction and specify the database and data privileges.
-const tx = todosDb.transaction('todos', 'readonly');
-
-// Open up the desired object store.
-const store = tx.objectStore('todos');
-
-// Use the .getAll() method to get all data in the database.
-const request = store.getAll();
-
- // Get confirmation of the request.
-const result = await request;
-console.log('result.value', result);
-return result;
+// Method that gets content from the IndexedDB database using the idb module
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  return result?.value;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,10 +2,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -19,27 +15,25 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        // Creates a copy of the index.html file in the dist folder and inserts in script tag to the newly created bundle.js file
-        template: './index.html', 
-        // Optional parameters
-        title: 'Text Editor'
+        template: './index.html',
+        title: 'Just Another Text Editor'
       }),
-
+      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
-      }), 
-
+      }),
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Just another text editor',
-        short_name: 'JATE',
-        description: 'Just another text editor!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Text Editor',
+        background_color: '#225CA3',
+        theme_color: '#225CA3',
+        start_url: './',
+        publicPath: './',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
@@ -48,11 +42,26 @@ module.exports = () => {
           },
         ],
       }),
-      ],
-
+    ],
     module: {
+      // CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
